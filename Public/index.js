8346808d368244dd679a2f2270c9d190d42df3c3
Robert V Frazier <robert.v.frazier@gmail.com>
2018-07-09 13:43:39 -0400

    Info button works; back button does not

--- Public/index.js
+++ Public/index.js
@@ -27,6 +27,20 @@ const generateHtml={
         let pageSplashHtml=
         $('div.js-pageViewSplashHtml').html(pageSplashHtml);
         $('div.js-pageViewSplashHtml').hide();
+    },
+
+    gameHtml: function(){
+        // console.log('In the gameHtml method.');
+        let pageGameHtml=
+        $('div.js-pageViewGameBoardHtml').html(pageGameHtml);
+        $('div.js-pageViewGameBoardHtml').hide();
+    },
+
+    infoHtml: function(){
+        // console.log('In the infoHtml method.');
+        let pageInfoHtml=
+        $('div.js-pageViewInstructionsHtml').html(pageInfoHtml);
+        $('div.js-pageViewInstructionsHtml').hide();
     }
 };
 
@@ -36,37 +50,68 @@ Step 1b: Render each HTML page, based on the current state.
 
 const renderPage={
    doShowPages: function(){
-        // console.log('In the doShowPages method.');
+        console.log('In the doShowPages method.');
         if(STORE.currentView==='splash'){
             this.splashPage();
+        }else if(STORE.currentView==='info'){
+            this.infoPage();
         }
    },
 
-    showCurrentPage: function(pageToShow, userButtonText){
-        // console.log('In the showCurrentPage method.');
+    showCurrentPage: function(pageToShow){
+        console.log('In the showCurrentPage method.');
         $('div.js-pageViewSplashHtml').hide();
+        $('div.js-pageViewInfoHtml').hide();
         $(pageToShow).show();
     },
 
     splashPage: function(){
-        // console.log('In the splashPage method.');
-        this.showCurrentPage('div.js-pageViewSplashHtml', 'Start');
-    }
+        console.log('In the splashPage method.');
+        this.showCurrentPage('div.js-pageViewSplashHtml');
+    },
+
+    infoPage: function(){
+        console.log('In the infoPage method.');
+        this.showCurrentPage('div.js-pageViewInfoHtml');
+    },
 };
 
 /******************************************************** 
  * Step 2: Listen for user interactions.
  ********************************************************/
 
-const listeners={
+const Listeners={
+    listen: function(){
+        console.log('In the listen method.');
+        this.handleInfoButton();
+        this.handleBackButtonInfoPage();
+    },
+
+    handleInfoButton: function(){
+        console.log('In the handleInfoButton method.');
+        $('.js-infoButton').on('click', function() {
+            STORE.currentView='info';
+            renderPage.doShowPages();
+        });
+    },
+
+    handleBackButtonInfoPage: function(){
+        console.log('In the handleBackButtonInfoPage method.');
+        $('.js-backButtonInfoPage').on('click', function() {
+            console.log('clicked the back button');
+            STORE.currentView='splash';
+            console.log(STORE.currentView);
+            renderPage.doShowPages();
+        });
+    },
 };
 
 /******************************************************** 
  * Step 3: Get API data to match user inputs.
  ********************************************************/
 
-const getFlickrPics={
-};
+// const getFlickrPics={
+// };
 
 /***************************** 
  * Javascript starts here.
@@ -74,8 +119,9 @@ const getFlickrPics={
 
 function main(){
     // console.log('Begin the program');
-    generateHtml.doHtmlPages();
+    // generateHtml.doHtmlPages();
     renderPage.doShowPages();
+    Listeners.listen();
 };
 
 $(main);
