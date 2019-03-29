2b43a6b84c6cdb0c53788bde7062bb7f82c754e4
Robert V Frazier <robert.v.frazier@gmail.com>
2019-03-29 15:03:56 -0400

    Refactoring to reduce repetition.

--- public_2/src/Components/ChessBoard/index.js
+++ public_2/src/Components/ChessBoard/index.js
@@ -37,6 +37,15 @@ export default class ChessBoard extends React.Component {
     return board;
   };
 
+  updateMove(moveType, position) {
+    this.setState({ endPosition: position }, () => {
+      const board = [...this.state.board];
+      this.updateBoardSquares(moveType, () =>
+        this.setState({ startPosition: position, board })
+      );
+    });
+  }
+
   handleSquareClicked = position => {
     const { startPosition } = this.state;
     if (startPosition === null) {
@@ -50,185 +59,87 @@ export default class ChessBoard extends React.Component {
       position - this.state.startPosition < 8 &&
       position % 8 > this.state.startPosition % 8
     ) {
-      this.setState({ endPosition: position }, () => {
-        const board = [...this.state.board];
-        this.updateBoardSquares("horizRight");
-        this.setState({ startPosition: position, board });
-      });
+      this.updateMove("horizRight", position);
     } else if (
       // legal horizontal move left
       this.state.startPosition > position &&
       this.state.startPosition - position < 8 &&
       this.state.startPosition % 8 > position % 8
     ) {
-      this.setState({ endPosition: position }, () => {
-        const board = [...this.state.board];
-        this.updateBoardSquares("horizLeft");
-        this.setState({ startPosition: position, board });
-      });
+      this.updateMove("horizLeft", position);
     } else if (
       // legal vertical move down
       this.state.startPosition < position &&
       this.state.startPosition % 8 === position % 8
     ) {
-      this.setState({ endPosition: position }, () => {
-        const board = [...this.state.board];
-        this.updateBoardSquares("vertDown");
-        this.setState({ startPosition: position, board });
-      });
+      this.updateMove("vertDown", position);
     } else if (
       // legal vertical move up
       this.state.startPosition > position &&
       this.state.startPosition % 8 === position % 8
     ) {
-      this.setState({ endPosition: position }, () => {
-        const board = [...this.state.board];
-        this.updateBoardSquares("vertUp");
-        this.setState({ startPosition: position, board });
-      });
+      this.updateMove("vertUp", position);
     } else if (
       // legal diagonal move down and right
       position > this.state.startPosition &&
       (position - this.state.startPosition) % 9 === 0
     ) {
-      this.setState({ endPosition: position }, () => {
-        const board = [...this.state.board];
-        this.updateBoardSquares("diagDownRight");
-        this.setState({ startPosition: position, board });
-      });
+      this.updateMove("diagDownRight", position);
     } else if (
       // legal diagonal move down and left
       position > this.state.startPosition &&
       (position - this.state.startPosition) % 7 === 0
     ) {
-      this.setState({ endPosition: position }, () => {
-        const board = [...this.state.board];
-        this.updateBoardSquares("diagDownLeft");
-        this.setState({ startPosition: position, board });
-      });
+      this.updateMove("diagDownLeft", position);
     } else if (
       // legal diagonal move up and left
       position < this.state.startPosition &&
       (this.state.startPosition - position) % 9 === 0
     ) {
-      this.setState({ endPosition: position }, () => {
-        const board = [...this.state.board];
-        this.updateBoardSquares("diagUpLeft");
-        this.setState({ startPosition: position, board });
-      });
+      this.updateMove("diagUpLeft", position);
     } else if (
       // legal diagonal move up and right
       position < this.state.startPosition &&
       (this.state.startPosition - position) % 7 === 0
     ) {
-      this.setState({ endPosition: position }, () => {
-        const board = [...this.state.board];
-        this.updateBoardSquares("diagUpRight");
-        this.setState({ startPosition: position, board });
-      });
+      this.updateMove("diagUpRight", position);
     }
   };
 
-  updateBoardSquares = moveType => {
+  condition = moveType => {
+    return (
+      {
+        horizRight: (i, start, end) => i >= start && i <= end,
+        horizLeft: (i, start, end) => i >= end && i <= start,
+        vertDown: (i, start, end) =>
+          i >= start && i <= end && i % 8 === start % 8,
+        vertUp: (i, start, end) =>
+          i <= start && i >= end && i % 8 === start % 8,
+        diagDownRight: (i, start, end) =>
+          i >= start && i <= end && (i - start) % 9 === 0,
+        diagDownLeft: (i, start, end) =>
+          i >= start && i <= end && (i - start) % 7 === 0,
+        diagUpLeft: (i, start, end) =>
+          i <= start && i >= end && (start - i) % 9 === 0,
+        diagUpRight: (i, start, end) =>
+          i <= start && i >= end && (start - i) % 7 === 0
+      }[moveType] || {}
+    );
+  };
+
+  updateBoardSquares = (moveType, cb) => {
     const { startPosition, endPosition } = this.state;
-    if (moveType === "horizRight") {
-      this.setState({
+    this.setState(
+      {
         board: this.state.board.map((square, i) => {
-          if (i >= startPosition && i <= endPosition) {
+          if (this.condition(moveType)(i, startPosition, endPosition)) {
             square.movedThrough = true;
           }
           return square;
         })
-      });
-    } else if (moveType === "horizLeft") {
-      this.setState({
-        board: this.state.board.map((square, i) => {
-          if (i >= endPosition && i <= startPosition) {
-            square.movedThrough = true;
-          }
-          return square;
-        })
-      });
-    } else if (moveType === "vertDown") {
-      this.setState({
-        board: this.state.board.map((square, i) => {
-          if (
-            i >= startPosition &&
-            i <= endPosition &&
-            i % 8 === startPosition % 8
-          ) {
-            square.movedThrough = true;
-          }
-          return square;
-        })
-      });
-    } else if (moveType === "vertUp") {
-      this.setState({
-        board: this.state.board.map((square, i) => {
-          if (
-            i <= startPosition &&
-            i >= endPosition &&
-            i % 8 === startPosition % 8
-          ) {
-            square.movedThrough = true;
-          }
-          return square;
-        })
-      });
-    } else if (moveType === "diagDownRight") {
-      this.setState({
-        board: this.state.board.map((square, i) => {
-          if (
-            i >= startPosition &&
-            i <= endPosition &&
-            (i - startPosition) % 9 === 0
-          ) {
-            square.movedThrough = true;
-          }
-          return square;
-        })
-      });
-    } else if (moveType === "diagDownLeft") {
-      this.setState({
-        board: this.state.board.map((square, i) => {
-          if (
-            i >= startPosition &&
-            i <= endPosition &&
-            (i - startPosition) % 7 === 0
-          ) {
-            square.movedThrough = true;
-          }
-          return square;
-        })
-      });
-    } else if (moveType === "diagUpLeft") {
-      this.setState({
-        board: this.state.board.map((square, i) => {
-          if (
-            i <= startPosition &&
-            i >= endPosition &&
-            (startPosition - i) % 9 === 0
-          ) {
-            square.movedThrough = true;
-          }
-          return square;
-        })
-      });
-    } else if (moveType === "diagUpRight") {
-      this.setState({
-        board: this.state.board.map((square, i) => {
-          if (
-            i <= startPosition &&
-            i >= endPosition &&
-            (startPosition - i) % 7 === 0
-          ) {
-            square.movedThrough = true;
-          }
-          return square;
-        })
-      });
-    }
-    console.log(this.state.startPosition, this.state.endPosition);
+      },
+      cb
+    );
   };
 
   render() {
