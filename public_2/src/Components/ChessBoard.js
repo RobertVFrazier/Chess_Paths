3a8c3871257d1aa220e0a120ffc82a1e0c36b27f
Robert V Frazier <robert.v.frazier@gmail.com>
2019-05-10 14:16:04 -0400

    Some progress with GSAP.

--- public_2/src/Components/ChessBoard.js
+++ public_2/src/Components/ChessBoard.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import queenStanding from '../Images/Queen_Standing.svg';
-import { initBoard, addMove, setPositions, highlightSquares, updateScoreboard } from '../Actions';
-import Square from './Square';
+import React from "react";
+import { connect } from "react-redux";
+import queenStanding from "../Images/Queen_Standing.svg";
+import {
+  initBoard,
+  addMove,
+  setPositions,
+  highlightSquares,
+  updateScoreboard
+} from "../Actions";
+import Square from "./Square";
 
 export class ChessBoard extends React.Component {
   constructor() {
@@ -22,29 +28,31 @@ export class ChessBoard extends React.Component {
 
   render() {
     return (
-      <ul className="board">
+      <div>
+        <ul className="board">
+          {this.props.board.map(square => (
+            <Square
+              queenContainer={this.queenContainer}
+              handleSquareClicked={this.handleSquareClicked}
+              key={square.position}
+              {...square}
+            />
+          ))}
+        </ul>
         <img
           src={queenStanding}
           alt=""
           className="queen"
           ref={img => (this.queenContainer = img)}
         />
-        {this.props.board.map(square => (
-          <Square
-            queenContainer={this.queenContainer}
-            handleSquareClicked={this.handleSquareClicked}
-            key={square.position}
-            {...square}
-          />
-        ))}
-      </ul>
+      </div>
     );
   }
 }
 
 const mapState = state => {
   return {
-    board: state.board,
+    board: state.board
   };
 };
 
