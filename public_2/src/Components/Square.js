ce08f2151e1989cf14da6958bb702db4e0c82e56
Robert V Frazier <robert.v.frazier@gmail.com>
2019-05-11 13:03:11 -0400

    Initial placement of queen on board is now instantaneous; moves take time.

--- public_2/src/Components/Square.js
+++ public_2/src/Components/Square.js
@@ -1,8 +1,15 @@
 import React from "react";
 import className from "classnames";
+import { connect } from "react-redux";
 import { TimelineLite } from "gsap/all";
 
-export default class Square extends React.Component {
+function mapStateToProps(state) {
+  return {
+    board: state.board
+  };
+}
+
+class Square extends React.Component {
   constructor() {
     super();
     this.initialSquareTween = new TimelineLite();
@@ -11,18 +18,20 @@ export default class Square extends React.Component {
   handleSquareClicked = event => {
     this.props.handleSquareClicked(event.currentTarget.value);
     // console.log(this.props);
+    const squaresDone = this.props.board.filter(tile => tile.movedThrough)
+      .length;
     let squareNumber = parseInt(this.props.position, 10);
     let tweenX = (squareNumber % 8) * 12.25 + "vw";
     let tweenY = 0 - (8 - Math.floor(squareNumber / 8)) * 12.25 + "vw";
-    console.log(tweenX, tweenY);
-    console.log(this.props.movesDone);
+    let delayTime = squaresDone === 1 ? 0.001 : 0.5;
+    // console.log(tweenX, tweenY);
     this.initialSquareTween
-      .to(this.props.queenContainer, 0.5, {
+      .to(this.props.queenContainer, delayTime, {
         x: tweenX,
         y: tweenY,
-        display: "block"
+        display: "block" // Where is it documented that this is needed? Why won't the queen appear without it?
       })
-      .to(this.props.queenContainer, 1.5, { opacity: 1 });
+      .to(this.props.queenContainer, 1.25, { opacity: 1 });
   };
 
   render() {
@@ -37,10 +46,12 @@ export default class Square extends React.Component {
         value={this.props.position}
         className={classname}
       >
-        {this.props.position}&nbsp;&nbsp;&nbsp;&nbsp;
+        {/* {this.props.position}&nbsp;&nbsp;&nbsp;&nbsp;
         {parseInt(this.props.position, 10) % 8}&nbsp;&nbsp;&nbsp;&nbsp;
-        {Math.floor(parseInt(this.props.position, 10) / 8)}
+        {Math.floor(parseInt(this.props.position, 10) / 8)} */}
       </li>
     );
   }
 }
+
+export default connect(mapStateToProps)(Square);
