6aae7f52d34935f452a8988ceafdd7385ec95a2f
Robert V Frazier <robert.v.frazier@gmail.com>
2019-05-12 16:13:17 -0400

    Queen movement time is now a function of how many squares moved. Fixed a bug in testing for legal diagonal moves. Added new animation when illegal move is attempted.

--- public_2/src/Components/Square.js
+++ public_2/src/Components/Square.js
@@ -1,37 +1,114 @@
 import React from "react";
 import className from "classnames";
 import { connect } from "react-redux";
-import { TimelineLite } from "gsap/all";
+import { TweenMax } from "gsap/all";
+
+let start = null;
 
 function mapStateToProps(state) {
   return {
-    board: state.board
+    board: state.board,
+    startPosition: state.startPosition
   };
 }
 
-class Square extends React.Component {
-  constructor() {
-    super();
-    this.initialSquareTween = new TimelineLite();
+function testForLegalMove(start, end, startColor, endColor) {
+  let moveType = null;
+  let moveSquares = 0;
+  if (start === null) {
+    moveType = "placePiece";
+  } else if (end > start && end - start < 8 && end % 8 > start % 8) {
+    moveType = "horizRight";
+    moveSquares = end - start;
+  } else if (start > end && start - end < 8 && start % 8 > end % 8) {
+    moveType = "horizLeft";
+    moveSquares = start - end;
+  } else if (start < end && start % 8 === end % 8) {
+    moveType = "vertDown";
+    moveSquares =
+      Math.floor(parseInt(end, 10) / 8) - Math.floor(parseInt(start, 10) / 8);
+  } else if (start > end && start % 8 === end % 8) {
+    moveType = "vertUp";
+    moveSquares =
+      Math.floor(parseInt(start, 10) / 8) - Math.floor(parseInt(end, 10) / 8);
+  } else if (
+    end > start &&
+    (end - start) % 9 === 0 &&
+    startColor === endColor
+  ) {
+    moveType = "diagDownRight";
+    moveSquares = (parseInt(end, 10) % 8) - (parseInt(start, 10) % 8);
+  } else if (
+    end > start &&
+    (end - start) % 7 === 0 &&
+    startColor === endColor
+  ) {
+    moveType = "diagDownLeft";
+    moveSquares = (parseInt(start, 10) % 8) - (parseInt(end, 10) % 8);
+  } else if (
+    end < start &&
+    (start - end) % 9 === 0 &&
+    startColor === endColor
+  ) {
+    moveType = "diagUpLeft";
+    moveSquares = (parseInt(start, 10) % 8) - (parseInt(end, 10) % 8);
+  } else if (
+    end < start &&
+    (start - end) % 7 === 0 &&
+    startColor === endColor
+  ) {
+    moveType = "diagUpRight";
+    moveSquares = (parseInt(end, 10) % 8) - (parseInt(start, 10) % 8);
   }
+  let squareTime = 0.15;
+  let time = moveSquares * squareTime;
+  return { moveType, time };
+}
+
+class Square extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.queenTween = new TimelineLite();
+  // }
 
   handleSquareClicked = event => {
-    this.props.handleSquareClicked(event.currentTarget.value);
-    // console.log(this.props);
-    const squaresDone = this.props.board.filter(tile => tile.movedThrough)
-      .length;
-    let squareNumber = parseInt(this.props.position, 10);
-    let tweenX = (squareNumber % 8) * 12.25 + "vw";
-    let tweenY = 0 - (8 - Math.floor(squareNumber / 8)) * 12.25 + "vw";
-    let delayTime = squaresDone === 1 ? 0.001 : 0.5;
-    // console.log(tweenX, tweenY);
-    this.initialSquareTween
-      .to(this.props.queenContainer, delayTime, {
+    const queen = this.props.queenContainer;
+    let end = event.currentTarget.value;
+    let { moveType, time } = testForLegalMove(
+      start,
+      end,
+      start === null ? null : this.props.board[start].black,
+      this.props.board[end].black
+    );
+    // console.log(
+    //   start,
+    //   end,
+    //   this.props.board[end].black ? "Black" : "White",
+    //   `${moveType}` === null ? "ILLEGAL!" : `${moveType}`
+    // );
+    if (`${moveType}` === null) {
+      TweenMax.to(queen, 0.15, { rotation: 6 })
+        .repeat(3)
+        .yoyo(true);
+    } else {
+      start = end;
+      this.props.handleSquareClicked(event.currentTarget.value);
+      // console.log(this.props);
+      const squaresDone = this.props.board.filter(tile => tile.movedThrough)
+        .length;
+      let squareNumber = parseInt(this.props.position, 10);
+      let tweenX = (squareNumber % 8) * 12.25 + "vw";
+      let tweenY = 0 - (8 - Math.floor(squareNumber / 8)) * 12.25 + "vw";
+      let delayTime = squaresDone === 1 ? 0.001 : `${time}`;
+      TweenMax.to(queen, delayTime, {
         x: tweenX,
         y: tweenY,
         display: "block" // Where is it documented that this is needed? Why won't the queen appear without it?
-      })
-      .to(this.props.queenContainer, 1.25, { opacity: 1 });
+      });
+      if (squaresDone === 1) {
+        TweenMax.to(queen, 1.25, { opacity: 1 });
+      }
+    }
   };
 
   render() {
